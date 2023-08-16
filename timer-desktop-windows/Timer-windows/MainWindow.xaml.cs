c404e708cc171f776fc737044c37e8538785b284
Matthew.nexuse <109680265+matio138@users.noreply.github.com>
2023-08-16 16:17:10 +0330

    desktop version stable

--- timer-desktop-windows/Timer-windows/MainWindow.xaml.cs
+++ timer-desktop-windows/Timer-windows/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Timers;
+using System.Windows.Threading;
 
 namespace Timer_countdown
 {
@@ -21,17 +23,32 @@ namespace Timer_countdown
     /// </summary>
     public partial class MainWindow : Window
     {
+        public DateTime TargetTime = new DateTime(2023, 12, 5, 12, 0, 0);
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer intervaltimer = new DispatcherTimer();
+            intervaltimer.Interval = TimeSpan.FromSeconds(1);
+            intervaltimer.IsEnabled = true;
+            intervaltimer.Tick += new EventHandler(TimerTick);
+            intervaltimer.Start();
         }
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
+        }
 
-            
+
+        public void TimerTick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            TimeSpan remining = TargetTime.Subtract(now);
+            day_timer.Text = Convert.ToString(remining.Days);
+            hour_timer.Text = Convert.ToString(remining.Hours);
+            minute_timer.Text = Convert.ToString(remining.Minutes);
+            second_timer.Text = Convert.ToString(remining.Seconds);
         }
     }
 }
